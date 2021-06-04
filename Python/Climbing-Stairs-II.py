aa82c3d4bced9356bc7344401b27f9a087726ccd
1thatcodes <ajaykrishnaanandhan@yahoo.com>
2021-06-04 21:24:08 +0530

    Update Climbing Stairs-II  with Recursion + Memoization

---
+++ Python/Climbing-Stairs-II.py
@@ -0,0 +1,22 @@
+# # 01
+# Recursion + Memoization
+# Time complexity : O(n) 
+# Space complexity : O(n) for memo
+
+# Accepted
+# 
+
+class Solution:
+    
+    memo = {}
+    def climbStairs(self, n: int) -> int:
+        
+        if n not in self.memo:
+            
+            if n < 3:
+                self.memo[n] = n
+                
+            else:
+                self.memo[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
+                  
+        return self.memo[n]
