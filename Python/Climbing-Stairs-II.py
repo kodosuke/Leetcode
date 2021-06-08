dbd6d387c00ea53cf357901881264eee951207cf
kodosuke <ajaykrishnaanandhan@yahoo.com>
2021-06-08 09:21:21 +0530

    Top Down Approach with Memoization

--- Python/Climbing-Stairs-II.py
+++ Python/Climbing-Stairs-II.py
@@ -5,18 +5,20 @@
 
 # Accepted
 # 
-
 class Solution:
     
     memo = {}
+    
     def climbStairs(self, n: int) -> int:
         
         if n not in self.memo:
-            
-            if n < 3:
-                self.memo[n] = n
-                
+            if n == -1:
+                self.memo[n] = 0
+            elif n == 0:
+                self.memo[n] = 1
             else:
                 self.memo[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
-                  
+                
         return self.memo[n]
+        
+        
