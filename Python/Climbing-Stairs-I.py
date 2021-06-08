3faa73432cb4224333e1c3bad887fce6cec4310f
kodosuke <ajaykrishnaanandhan@yahoo.com>
2021-06-08 08:58:38 +0530

    Top Down Approach

--- Python/Climbing-Stairs-I.py
+++ Python/Climbing-Stairs-I.py
@@ -1,5 +1,5 @@
 # # 70
-# Recursion
+# Top Down Approach - Recursion
 # Time complexity : O(2^n) 
 # Size of recursion tree will be 2^n
 # Space complexity : O(n)
@@ -9,6 +9,8 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         
-        if n <= 2:
-            return n
+        if n == -1:
+            return 0
+        elif n == 0:
+            return 1
         return self.climbStairs(n - 1) + self.climbStairs(n - 2)
